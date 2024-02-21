7cee51b1b7a6d400aa6e89e47a4a6843009e5e60
TashafMahmood <tashaf@elred.io>
2024-02-21 17:26:23 +0530

    next js project

---
+++ firstapp/src/app/page.js
@@ -0,0 +1,19 @@
+'use client'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Homepage</h1>
+      <User username={'Tashaf'} />
+      <User username={'Atique'} />
+      <button onClick={() => alert("hello")}>Click me</button>
+    </main>
+  )
+}
+
+const User = ({ username }) => {
+  return (
+    <div>My name is {username}</div>
+  )
+}
