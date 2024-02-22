ba60e3a1f3426ec4743ae33addd8b3ef46a2984d
TashafMahmood <tashaf@elred.io>
2024-02-22 12:31:07 +0530

    conditional layout

--- firstapp/src/app/login/layout.js
+++ firstapp/src/app/login/layout.js
@@ -1,20 +1,27 @@
 'use client'
 import Link from "next/link"
 import './login.css'
-import { useRouter } from "next/navigation"
+import { useRouter, usePathname } from "next/navigation"
+
 
 const Layout = ({ children }) => {
     const router = useRouter()
+    const pathName = usePathname()
+
     return (
         <div>
-            <div className="login_nav">
-                <h4 onClick={() => router.push("/")}>Login Navigation</h4>
-                <ul>
-                    <Link href={'/login'}>Login</Link>
-                    <Link href={'/login/loginstudent'}>Student Login</Link>
-                    <Link href={'/login/loginteacher'}>Teacher Login</Link>
-                </ul>
-            </div>
+            {
+                pathName === "/login/loginteacher" ?
+                    <Link href={'/login'}>Go to Login Main</Link> :
+                    <div className="login_nav">
+                        <h4 onClick={() => router.push("/")}>Login Navigation</h4>
+                        <ul>
+                            <Link href={'/login'}>Login</Link>
+                            <Link href={'/login/loginstudent'}>Student Login</Link>
+                            <Link href={'/login/loginteacher'}>Teacher Login</Link>
+                        </ul>
+                    </div>
+            }
             {children}
         </div>
     )
