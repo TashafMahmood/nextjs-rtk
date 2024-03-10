93ef66348540f4372edc45fd656fcdb82fc760d8
TashafMahmood <tashaf@elred.io>
2024-03-10 23:29:17 +0530

    image

--- firstapp/src/app/page.js
+++ firstapp/src/app/page.js
@@ -2,12 +2,17 @@
 import Link from 'next/link'
 import style from './page.module.css'
 import { useRouter } from 'next/navigation'
+import Image from 'next/image'
+import img1 from '../../public/vercel.svg'
 
 export default function Home() {
   const router = useRouter()
   return (
     <main >
       <h1 className={style.main}>Homepage</h1>
+      <Image
+        src={img1}
+      />
       <Link href={'/login'}>Go to Login</Link>
       <br />
       <br />
@@ -21,6 +26,9 @@ export default function Home() {
       <br />
       <br />
       <button onClick={() => router.push('/login')}>Go to login</button>
+      <br />
+      <br />
+
     </main>
   )
 }
