dc08bd5888fe14b85e2fb8a0435b1b2037702245
TashafMahmood <tashaf@elred.io>
2024-03-10 23:06:30 +0530

    modular css

--- firstapp/src/app/page.js
+++ firstapp/src/app/page.js
@@ -1,13 +1,13 @@
 'use client'
 import Link from 'next/link'
-import styles from './page.module.css'
+import style from './page.module.css'
 import { useRouter } from 'next/navigation'
 
 export default function Home() {
   const router = useRouter()
   return (
-    <main className={styles.main}>
-      <h1>Homepage</h1>
+    <main >
+      <h1 className={style.main}>Homepage</h1>
       <Link href={'/login'}>Go to Login</Link>
       <br />
       <br />
