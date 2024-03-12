b2f5343cad5172f7c51032f4735ebd4d62715b4f
TashafMahmood <tashaf@elred.io>
2024-03-12 09:55:35 +0530

    post api

--- firstapp/src/app/api/users/route.js
+++ firstapp/src/app/api/users/route.js
@@ -1,7 +1,15 @@
 import { users } from "@/util/db";
 import { NextResponse } from "next/server";
 
-export function GET(request,res) {
+export function GET(request, res) {
     const data = users
     return NextResponse.json(data, { status: 200 })
+}
+
+export async function POST(request) {
+    let payload = await request.json();
+    if (!payload.name) {
+        return NextResponse.json({ error: "please enter the required field", success: false }, { status: 400 })
+    }
+    return NextResponse.json({ result: "user created successfully", success: true }, { status: 201 })
 }
