5ef7ebab5783e7cceed95e63e31e0352edc80925
TashafMahmood <tashaf@elred.io>
2024-03-12 11:17:45 +0530

    put request

--- firstapp/src/app/api/users/[id]/route.js
+++ firstapp/src/app/api/users/[id]/route.js
@@ -6,3 +6,12 @@ export function GET(request, res) {
     const filterUser = data.filter(user => user.id == res.params.id);
     return NextResponse.json(filterUser.length == 0 ? { result: "No data found" } : { data: filterUser[0] }, { status: 200 })
 }
+
+export async function PUT(request, content) {
+    let payload = await request.json();
+    let id = await content.params.id
+    payload.id = id
+    console.log(payload.name)
+    console.log(id)
+    return NextResponse.json({ result: payload })
+}
