5c822b253a8737982cbd8732756275069cd32799
TashafMahmood <tashaf@elred.io>
2024-03-14 12:24:13 +0530

    sending data to redux store

--- nextjs-rtk/src/app/components/AddUser.js
+++ nextjs-rtk/src/app/components/AddUser.js
@@ -1,11 +1,21 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import { addUser } from '../redux/slice'
+import { useDispatch } from 'react-redux'
+
 
 const AddUser = () => {
+    const [user, setUser] = useState('')
+    const dispatch = useDispatch()
+    const addUserFunc = () => {
+        console.log(user)
+        dispatch(addUser(user))
+    }
     return (
         <div className='add-user'>
             <h2>User List</h2>
-            <input placeholder='Add User' type='text' className='add-user-input' />
-            <button className='add-user-btn'>Add user</button>
+            <input placeholder='Add User' type='text' className='add-user-input' value={user} onChange={e => setUser(e.target.value)} />
+            <button className='add-user-btn' onClick={addUserFunc}>Add user</button>
         </div>
     )
 }
